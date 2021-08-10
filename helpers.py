22efead8a5ada1af2c739e9ea4ad8caea17c9ef8
NateNate60 <nathanlim2004@gmail.com>
2021-08-10 06:34:49 +0000

    Add helpers

--- helpers.py
+++ helpers.py
@@ -3,6 +3,7 @@ This file contains helper functions.
 """
 
 import time
+import json
 
 def journal (message: str) :
     """
@@ -11,3 +12,17 @@ def journal (message: str) :
     t = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
     with open ("log.txt", 'a') as f :
         f.write(t + ": " + message + '\n')
+
+def readchecked () -> list :
+    """
+    Read the list of the checked posts from json file
+    """
+    with open ("checked.json", 'r') as f :
+        return json.load(f)
+
+def writechecked (checked: list) -> None :
+    """
+    Write the list of the checked posts to json file
+    """
+    with open ("checked.json", 'w') as f :
+        json.dump(checked, f)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from prawcore import exceptions
+from helpers import readchecked, writechecked
 import actions
 import config
 import userdb
@@ -9,6 +10,7 @@ def main() :
         db = userdb.Database()
         r = actions.logon()
         while (True) :
+            checked = readchecked()
             for message in r.inbox.unread() :
                 b = message.body.lower()
                 if ('u/c4c_bot' in b) :
@@ -27,6 +29,18 @@ def main() :
                     except ValueError :
                         message.reply("Confirmation failed. Please ensurse you are the person mentioned in the parent comment.")
                     message.mark_read()
+            for post in r.subreddit("cash4cash").new(limit=5) :
+                if (post.id in checked) :
+                    continue
+                author = db.lookup(post.author.name)
+                message = "#OP's Cash4Cash Trust Score: " +   author.score + '\n\n'
+                if (author.score < 200) :
+                    message += "Note: This user's trust score is low. It's highly recommended to use the escrow when trading, since a large portion of scams target or are perpetrated by such users.\n\n"
+                
+                message += "➤ Automated escrow service: Don't like the uncertainty of going first? Want to make sure the other party doesn't take your money and disappear? Our escrow service can help with that. [Click here to start a new escrow transaction with this user.](https://www.reddit.com/message/compose?to=C4CSwapBot&subject=Escrow&message=--NEW TRANSACTION--%0APartner%3A {{author}}%0AAmount%3A 0.12345 BTC%2FBCH%0A--CONTRACT--%0AWrite whatever you want here. What are the parties agreeing to%3F%0AAbout this service%3A https%3A%2F%2Fwww.reddit.com%2Fr%2FCash4Cash%2Fwiki%2Fedit%2Findex%2Fescrow)  ([about](https://reddit.com/r/Cash4Cash/wiki/index/escrow))\n\n"
+                message += "➤ Trade safely! **Scammers will contact you via PM & reddit chat**: they may pretend to be experienced traders with fake rep pages, or pose as the person you're negotiating with. It falls on you to do your own due diligence.\n\n"
+                message += "➤ Please **[read all of our rules](https://www.reddit.com/r/Cash4Cash/wiki/index)**. We require any user to comment on your post first before contacting you. It's one of the easiest ways to check for a scam. If someone won't comment on your post **DO NOT WORK WITH THEM** and report them to moderators.\n\n"
+                message += "➤ For your convenience, upon conclusion of this trade please post a `!closed` comment"
     except exceptions.ServerError :
         time.sleep(5)
         main()
