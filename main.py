093acac3059dcfacf78e3a7381eef7c0a9b93946
NateNate60 <nathanlim2004@gmail.com>
2021-08-17 05:08:25 +0000

    Lock initial confirmation comment

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main() :
                 b = message.body.lower()
                 if ('u/c4c_bot' in b) :
                     partner = actions.parseparter(b)
+                    message.mod().lock()
                     message.reply("PENDING CONFIRMATION\n\nWaiting for u/" +  partner + " to confirm the trade.\n\n**Note:** The flairing system has changed. You must reply to **this comment** to confirm! **If you reply to the parent comment your confirmation will not be detected." + config.signature)
                     message.mark_read()
                 if ("confirm" in b and "not" not in b) :
