32c8d481e4b35d217102eef83ada56609741acc0
NateNate60 <nathanlim2004@gmail.com>
2021-09-11 01:02:29 +0000

    Modify point formula (more generous)

--- actions.py
+++ actions.py
@@ -63,18 +63,18 @@ def log (r, usera: praw.models.Redditor, userb: praw.models.Redditor, amt: int, 
         elif (b.score <= 500) :
             basescore = 10
         elif (b.score <= 1000) :
-            basescore = 15
-        elif (b.score <= 2000) :
             basescore = 20
+        elif (b.score <= 2000) :
+            basescore = 30
         else :
-            basescore = 25
+            basescore = 35
     else :
         if (b.score <= 100) :
             basescore = 5
         elif (b.score <= 500) :
-            basescore = 10
+            basescore = 15
         elif (b.score <= 1000) :
-            basescore = 20
+            basescore = 30
         else :
             basescore = 40
     if (amt < 100) :
